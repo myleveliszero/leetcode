dacc35156c86acf4935bd55f9eb44eb0326e3d6f
myleveliszero <120106705+myleveliszero@users.noreply.github.com>
2023-01-11 20:26:44 +0500

    status: good

--- Basics/DesignParkingSystem_1603.py
+++ Basics/DesignParkingSystem_1603.py
@@ -1,11 +1,35 @@
-# Status: 
+# Status: Good 
 
 class ParkingSystem:
 
     def __init__(self, big: int, medium: int, small: int):
-        pass
+        self.big = big
+        self.medium = medium
+        self.small = small
 
     def addCar(self, carType: int) -> bool:
-        pass
+        if carType == 1:
+            if self.big:
+                self.big -= 1
+            else:
+                return False
+        elif carType == 2:
+            if self.medium:
+                self.medium -= 1
+            else:
+                return False
+        else:
+            if self.small:
+                self.small -= 1
+            else:
+                return False
+        return True
 
-solve = ParkingSystem()
+
+solve = ParkingSystem(2, 1, 1)
+print(solve.addCar(1), end=" ")
+print(solve.addCar(2), end=" ")
+print(solve.addCar(2), end=" ")
+print(solve.addCar(3), end=" ")
+print(solve.addCar(3), end=" ")
+print(solve.addCar(1))
