2da0cd42a7ba1f3ea22fd32e4e1c5c57c15232af
myleveliszero <120106705+myleveliszero@users.noreply.github.com>
2023-01-11 11:36:29 +0500

    added _1929

---
+++ Basics/ConcatenationOfArray_1929.py
@@ -0,0 +1,25 @@
+# Status: Perfect
+class Solution:
+    def getConcatenation(self, nums: list[int]) -> list[int]:
+       # return nums*2
+       # return nums+nums
+        nums.extend(nums)
+        return nums
+    def version2(self, nums: list[int]) -> list[int]:
+        for i in range(len(nums)):
+            nums.append(nums[i])
+        return nums
+       # for i in nums:
+       #     nums.append(i) # loop will be infinite 
+       # return nums 
+
+solve = Solution()
+print(solve.getConcatenation([1,2,3]))
+print(solve.version2([1,1,2]))
+print(solve.version2([0,0,1,1,2]))
+# Input: nums = [1,2,1]
+# Output: [1,2,1,1,2,1]
+# Input: nums = [1,1]
+# Output: [1,1,1,1]
+
+
