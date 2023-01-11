7e5fdbff19fcb4732dabe7a6f7b5e8815a6556b2
myleveliszero <120106705+myleveliszero@users.noreply.github.com>
2023-01-11 20:54:21 +0500

    status: bad

---
+++ Basics/StrictlyPalindromicNumber_2396.py
@@ -0,0 +1,9 @@
+# Status: Bad
+
+class Solution:
+    def isStrictlyPalindromic(self, n: int) -> bool:
+        return False
+
+solve = Solution()
+print(solve.isStrictlyPalindromic(9))
+print(solve.isStrictlyPalindromic(5))
