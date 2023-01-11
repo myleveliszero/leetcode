01a8eb5ece766aa19bc78ebaea46415ae07c7adf
myleveliszero <120106705+myleveliszero@users.noreply.github.com>
2023-01-11 12:31:05 +0500

    modified _1920

--- Basics/BuildArrayFromPermutation_1920.py
+++ Basics/BuildArrayFromPermutation_1920.py
@@ -2,7 +2,7 @@
 
 # Input: nums = [0,2,1,5,3,4]
 # Output: [0,1,2,4,5,3]
-# Explanation: answ = [0,1,2,4,5,3] = nums[nums[i]], 0 <= i,nums[i] < len
+# Explanation: answ = [0,1,2,4,5,3] = nums[nums[i]], 0 <= i,nums[i] < nums.length
 # Input: nums = [0,3,1,2]
 # Output: [0,2,3,1]
 
