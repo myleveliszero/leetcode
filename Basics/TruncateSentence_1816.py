4cb0bac421ca5550d3646e89d78bae2eda6e3b7b
myleveliszero <120106705+myleveliszero@users.noreply.github.com>
2023-01-15 21:15:28 +0500

    status: good

--- Basics/TruncateSentence_1816.py
+++ Basics/TruncateSentence_1816.py
@@ -21,6 +21,13 @@ class Solution:
                 countk1 += 1
             answidx += 1
         return s[:answidx]
+    def version2point2(self, s, k):
+        for idx, val in enumerate(s):
+            if val == " ":
+                k -= 1
+                if k == 0:
+                    return s[:idx]
+        return s
 
     def version3(self, s, k):
         return " ".join(s.split(maxsplit=k)[:k])
