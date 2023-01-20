f64ad1d0ceeb4a60bd68125d38a887cb5f9f2d87
myleveliszero <120106705+myleveliszero@users.noreply.github.com>
2023-01-20 10:38:51 +0500

    status: good

---
+++ Basics/NumberOfStringsThatAppearAsSubstringsInWord_1967.py
@@ -0,0 +1,18 @@
+# Status: Good
+
+class Solution:
+    def numOfStrings(self, patterns: list[str], word: str) -> int:
+        """
+        if patterns[i] is substring of word: answ += 1
+        """
+        answ = 0
+        for i in range(len(patterns)):
+            if patterns[i] in word:
+                answ += 1
+        return answ
+    def version2(self, patterns, word):
+        return sum(pattern in word for pattern in patterns)
+
+solve = Solution()
+print(solve.numOfStrings(patterns = ["a","abc","bc","d"], word = "abc"))
+print(solve.version2(patterns = ["a","abc","bc","d"], word = "abc"))
