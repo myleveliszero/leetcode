884394e2e823bc0df16ba0b666cad722f6ca2983
myleveliszero <120106705+myleveliszero@users.noreply.github.com>
2023-01-20 11:03:24 +0500

    status: good

---
+++ Basics/MatrixDiagonalSum_1572.py
@@ -0,0 +1,20 @@
+# Status: Good
+
+class Solution:
+    def diagonalSum(self, mat: list[list[int]]) -> int:
+        answ = 0
+        mlen = len(mat)
+        for i in range(mlen):
+            answ += mat[i][i] + mat[i][~i]
+        return answ if mlen%2 == 0 else answ - mat[mlen//2][mlen//2]
+    def version2(self, mat):
+        answ, mlen = 0, len(mat)
+        mid = mlen//2
+        for i in range(mid):
+            answ += mat[i][i]+mat[i][~i]+mat[~i][~i]+mat[~i][i]  
+        return answ if mlen%2 == 0 else answ + mat[mid][mid]
+
+solve = Solution()
+print(solve.diagonalSum(mat = [ [1,2,3],
+                                [4,5,6],
+                                [7,8,9]]))
