26017b1784676d2c800d0aafb3a07fe9a9e9f087
myleveliszero <120106705+myleveliszero@users.noreply.github.com>
2023-01-24 13:04:44 +0500

    status: good

--- Basics/FindGreatestCommonDivisorOfArray_1979.py
+++ Basics/FindGreatestCommonDivisorOfArray_1979.py
@@ -10,6 +10,18 @@ class Solution:
                 mx -= mn
         return mn
     def version2(self, nums):
+        mn, mx = nums[0], nums[0]
+        for i in range(len(nums)):
+            if nums[i] > mx:
+                mx = nums[i]
+            if nums[i] < mn:
+                mn = nums[i]
+        i = mn
+        while i != 0:
+            if mx%i==0 and mn%i==0:
+                return i
+            i -= 1
+    def version3(self, nums):
         import math
         return math.gcd(min(nums), max(nums))
 
@@ -18,3 +30,4 @@ print(solve.findGCD(nums = [2,5,6,9,10]))
 print(solve.findGCD(nums = [230, 10,30,23]))
 print(solve.version2(nums = [2,5,6,9,10]))
 print(solve.version2(nums = [230, 10,30,23]))
+print(solve.version3(nums = [230, 10,30,23]))
