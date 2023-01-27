99c7c70e5f0c86cbdf8030c912bc34e1f179c3b9
myleveliszero <120106705+myleveliszero@users.noreply.github.com>
2023-01-27 17:16:51 +0500

    status: good

--- Binary_Search/ValidPerfectSquare_367.py
+++ Binary_Search/ValidPerfectSquare_367.py
@@ -14,7 +14,7 @@ class Solution:
             elif isperfect >= num:
                 right = mid-1
         return False
-    def version2(self, num: int) -> bool:
+    def isPSqrtVersion2(self, num: int) -> bool:
         left, right = 0, num
         while left <= right:
             mid = left + (right - left)//2
@@ -26,23 +26,35 @@ class Solution:
             elif isperfect >= num:
                 right = mid-1
         return False
-
     def Newton(self, num):
         x = num
         while (x*x > num):
             x = (x+num/x)//2
         return x*x == num
+    def oddsequence(self, num):
+        # 1 + 3 + 5 + 7
+        #   4   9   16
+        i = 1
+        while (num > 0):
+            num -= i
+            i += 2
+        return num == 0
 
 solve = Solution()
-print(solve.isPerfectSquare(4))
-print(solve.isPerfectSquare(15))
-print(solve.isPerfectSquare(153))
-print(solve.isPerfectSquare(169))
-print(solve.isPerfectSquare(2147483647))
+print(solve.isPerfectSquare(4), end = " --> ")
+print(solve.isPerfectSquare(15), end = " --> ")
+print(solve.isPerfectSquare(153), end = " --> ")
+print(solve.isPerfectSquare(169), end = " --> ")
+print(solve.isPerfectSquare(2147483647), end = " --> ")
 print(solve.isPerfectSquare(1073741824))
-print(solve.Newton(153))
+
+print(solve.Newton(153), end = " --> ")
 print(solve.Newton(169))
-print(solve.version2(153))
-print(solve.version2(169))
-print(solve.version2(2147483647))
-print(solve.version2(1073741824))
+
+print(solve.isPSqrtVersion2(153), end = " --> ")
+print(solve.isPSqrtVersion2(169), end = " --> ")
+print(solve.isPSqrtVersion2(2147483647), end = " --> ")
+print(solve.isPSqrtVersion2(1073741824), end = " --> ")
+
+print(solve.oddsequence(153), end = " --> ")
+print(solve.oddsequence(169))
