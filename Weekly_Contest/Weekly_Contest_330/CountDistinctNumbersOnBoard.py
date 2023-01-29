e9326c857d23aa9b78f2528c43446efbc79f0ec5
myleveliszero <120106705+myleveliszero@users.noreply.github.com>
2023-01-29 08:11:56 +0500

    contest

---
+++ Weekly_Contest/Weekly_Contest_330/CountDistinctNumbersOnBoard.py
@@ -0,0 +1,9 @@
+class Solution:
+    def distinctIntegers(self, n: int) -> int:
+        return n-1 if n != 1 else 1
+
+solve = Solution()
+print(solve.distinctIntegers(6))
+print(solve.distinctIntegers(5))
+print(solve.distinctIntegers(3))
+print(solve.distinctIntegers(1))
