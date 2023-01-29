872d3034cccc41c71025f3f4447d24ed12c55085
myleveliszero <120106705+myleveliszero@users.noreply.github.com>
2023-01-29 08:15:15 +0500

    contest

--- Weekly_Contest/Weekly_Contest_330/CountDistinctNumbersOnBoard.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def distinctIntegers(self, n: int) -> int:
-        return n-1 if n != 1 else 1
-
-solve = Solution()
-print(solve.distinctIntegers(6))
-print(solve.distinctIntegers(5))
-print(solve.distinctIntegers(3))
-print(solve.distinctIntegers(1))
