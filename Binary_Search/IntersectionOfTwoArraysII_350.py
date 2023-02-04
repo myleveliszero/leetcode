26718fd49ac1ef34f4c538b58d51eeea0946b4f4
myleveliszero <120106705+myleveliszero@users.noreply.github.com>
2023-02-04 20:27:12 +0500

    updated

---
+++ Binary_Search/IntersectionOfTwoArraysII_350.py
@@ -0,0 +1,6 @@
+# Status:
+
+class Solution:
+    def intersect(self, nums1: list[int], nums2: list[int]) -> list[int]:
+        ans = 0
+
--- Biweekly_Contest/96/MinimumCommonValue.py
+++ Biweekly_Contest/96/MinimumCommonValue.py
@@ -1,5 +1,5 @@
 class Solution:
-    def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
+    def getCommon(self, nums1: list[int], nums2: list[int]) -> int:
         for i in range(len(nums1)):
             for j in range(len(nums2)):
                 if nums1[i] == nums2[j]:
