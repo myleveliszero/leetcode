65c1bc3872b27aa395221c83b4802587ffb23b83
myleveliszero <120106705+myleveliszero@users.noreply.github.com>
2023-02-04 20:33:46 +0500

    status: bad

--- Binary_Search/IntersectionOfTwoArraysII_350.py
+++ Binary_Search/IntersectionOfTwoArraysII_350.py
@@ -1,6 +1,14 @@
-# Status:
+# Status: Bad
 
 class Solution:
     def intersect(self, nums1: list[int], nums2: list[int]) -> list[int]:
-        ans = 0
+        ans = []
+        for i in nums1:
+            if i in nums2:
+                nums2.remove(i)
+                ans.append(i)
+        return ans
 
+solve = Solution()
+print(solve.intersect(nums1 = [1,2,2,1], nums2 = [2,2]))
+print(solve.intersect(nums1 = [4,9,5], nums2 = [9,4,9,8,4]))
