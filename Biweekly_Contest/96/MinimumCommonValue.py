1ead7e7e894784fde2f2ffa4b832bc9ceb7672bf
myleveliszero <120106705+myleveliszero@users.noreply.github.com>
2023-02-07 19:22:42 +0500

    added two pointers

--- Biweekly_Contest/96/MinimumCommonValue.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def getCommon(self, nums1: list[int], nums2: list[int]) -> int:
-        for i in range(len(nums1)):
-            for j in range(len(nums2)):
-                if nums1[i] == nums2[j]:
-                    return nums1[i]
-                elif nums1[i] < nums2[j]:
-                    break
-        return -1
