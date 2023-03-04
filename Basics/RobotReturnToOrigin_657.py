9d6b10daa406d2724b09f872412bad2cafd56a9b
myleveliszero <120106705+myleveliszero@users.noreply.github.com>
2023-03-04 20:10:55 +0500

    status: good

--- Basics/RobotReturnToOrigin_657.py
+++ Basics/RobotReturnToOrigin_657.py
@@ -2,18 +2,18 @@
 
 class Solution:
     def judgeCircle(self, moves: str) -> bool:
-        l,r,u,d = 0,0,0,0
+        l,d = 0,0
         for i in moves:
             if i == 'L':
                 l+=1
             elif i == 'R':
-                r+=1
+                l-=1
             elif i == 'U':
-                u+=1
+                d-=1
             else:
                 d+=1
-        return True if l==r and u==d else False
-
+        return True if l==0 and d==0 else False
+    
 solve = Solution()
 print(solve.judgeCircle("LLLLRRRRUDUDDUR"))  
 print(solve.judgeCircle("UD"))
