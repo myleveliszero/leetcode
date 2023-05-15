42fde195d2ee81b16c50f85e483526a76e324452
myleveliszero <120106705+myleveliszero@users.noreply.github.com>
2023-05-15 11:46:20 +0500

    [blank]

---
+++ Basics/SumMultiples_2652Easy.py
@@ -0,0 +1,10 @@
+class Solution:
+    def sumOfMultiples(self, n: int) -> int:
+        ans = []
+        for i in range(1, n+1):
+            if i % 3 == 0 or i % 5 == 0 or i % 7 == 0:
+                ans.append(i)
+        return sum(ans)
+
+solve = Solution()
+print(solve.sumOfMultiples(n = 7))
